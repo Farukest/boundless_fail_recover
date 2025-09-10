db00b9db4513984116ff3e7997e69e15d2d84730
Farukest <abdullahfarukozden@gmail.com>
2025-09-10 04:33:05 +0300

    changes

---
+++ lib/steel/contracts/src/OpSteel.sol
@@ -0,0 +1,95 @@
+// Copyright 2025 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.9;
+
+import {Encoding, Steel} from "./Steel.sol";
+
+/// @notice Validate an OP L2 block commitment, enabling verification on L1 of Steel queries against L2 state.
+/// @dev Application developers should inherit from this contract, and set the `optimismPortal` address
+/// to point to the portal contract of the OP L2 that will be the target for Steel queries.
+abstract contract OpCommitmentValidator {
+    /// @notice Address of the OptimismPortal2 contract.
+    IOptimismPortal2 public immutable optimismPortal;
+
+    constructor(address _optimismPortal) {
+        optimismPortal = IOptimismPortal2(_optimismPortal);
+    }
+
+    /// @notice Validates a Steel commitment.
+    /// @param commitment The commitment to validate.
+    /// @param configID The expected configID for the commitment. The configID commits to the chain
+    ///        specification used to instantiate the EVM within Steel.
+    /// @return True if the commitment is valid, false otherwise.
+    function validateCommitmentWithConfig(Steel.Commitment memory commitment, bytes32 configID)
+        internal
+        view
+        returns (bool)
+    {
+        (uint240 blockID, uint16 version) = Encoding.decodeVersionedID(commitment.id);
+        if (version == 0x100) {
+            if (configID != commitment.configID) {
+                revert Steel.InvalidConfigID(configID, commitment.configID);
+            }
+            return validateDisputeGameCommitment(blockID, commitment.digest);
+        } else {
+            return Steel.validateCommitmentWithConfig(commitment, configID);
+        }
+    }
+
+    /// @notice Validates a Dispute Game commitment.
+    /// @param gameIndex The index of the game in the DisputeGameFactory.
+    /// @param rootClaim The root claim of the dispute game.
+    /// @return True if the commitment is valid, false otherwise.
+    function validateDisputeGameCommitment(uint256 gameIndex, bytes32 rootClaim) internal view returns (bool) {
+        IDisputeGameFactory factory = optimismPortal.disputeGameFactory();
+        IAnchorStateRegistry anchorStateRegistry = optimismPortal.anchorStateRegistry();
+
+        // Retrieve game information from the factory.
+        (,, IDisputeGame game) = factory.gameAtIndex(gameIndex);
+
+        // Check that the root claim is valid.
+        if (!anchorStateRegistry.isGameClaimValid(game)) return false;
+
+        // Finally, verify that the provided root claim matches the game's root claim.
+        return game.rootClaim() == rootClaim;
+    }
+}
+
+// https://github.com/ethereum-optimism/optimism/blob/op-contracts/v4.0.0/packages/contracts-bedrock/interfaces/L1/IOptimismPortal2.sol
+interface IOptimismPortal2 {
+    function anchorStateRegistry() external view returns (IAnchorStateRegistry);
+    function disputeGameFactory() external view returns (IDisputeGameFactory);
+}
+
+// https://github.com/ethereum-optimism/optimism/blob/op-contracts/v4.0.0/packages/contracts-bedrock/interfaces/dispute/IAnchorStateRegistry.sol
+interface IAnchorStateRegistry {
+    function isGameClaimValid(IDisputeGame _game) external view returns (bool);
+}
+
+// https://github.com/ethereum-optimism/optimism/blob/op-contracts/v4.0.0/packages/contracts-bedrock/interfaces/dispute/IDisputeGameFactory.sol
+interface IDisputeGameFactory {
+    function gameCount() external view returns (uint256);
+    function gameAtIndex(uint256 _index)
+        external
+        view
+        returns (uint32 gameType_, uint64 timestamp_, IDisputeGame proxy_);
+}
+
+// https://github.com/ethereum-optimism/optimism/blob/op-contracts/v4.0.0/packages/contracts-bedrock/interfaces/dispute/IDisputeGame.sol
+interface IDisputeGame {
+    function rootClaim() external pure returns (bytes32 rootClaim_);
+}
---
+++ lib/steel/crates/steel/src/ethereum.rs
@@ -0,0 +1,230 @@
+// Copyright 2025 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Type aliases and specifications for Ethereum.
+use crate::{
+    config::{ChainSpec, ForkCondition},
+    serde::RlpHeader,
+    EvmBlockHeader, EvmEnv, EvmFactory, EvmInput,
+};
+use alloy_eips::{eip4844, eip7691};
+use alloy_evm::{Database, EthEvmFactory as AlloyEthEvmFactory, EvmFactory as AlloyEvmFactory};
+use alloy_primitives::{Address, BlockNumber, Bytes, TxKind, B256, U256};
+use revm::{
+    context::{BlockEnv, CfgEnv, TxEnv},
+    context_interface::block::BlobExcessGasAndPrice,
+    inspector::NoOpInspector,
+    primitives::hardfork::SpecId,
+};
+use serde::{Deserialize, Serialize};
+use std::{collections::BTreeMap, error::Error, sync::LazyLock};
+
+/// The Ethereum Sepolia [ChainSpec].
+pub static ETH_SEPOLIA_CHAIN_SPEC: LazyLock<EthChainSpec> = LazyLock::new(|| ChainSpec {
+    chain_id: 11155111,
+    forks: BTreeMap::from([
+        (SpecId::MERGE, ForkCondition::Block(1735371)),
+        (SpecId::SHANGHAI, ForkCondition::Timestamp(1677557088)),
+        (SpecId::CANCUN, ForkCondition::Timestamp(1706655072)),
+        (SpecId::PRAGUE, ForkCondition::Timestamp(1741159776)),
+    ]),
+});
+
+/// The Ethereum Holešky [ChainSpec].
+pub static ETH_HOLESKY_CHAIN_SPEC: LazyLock<EthChainSpec> = LazyLock::new(|| ChainSpec {
+    chain_id: 17000,
+    forks: BTreeMap::from([
+        (SpecId::MERGE, ForkCondition::Block(0)),
+        (SpecId::SHANGHAI, ForkCondition::Timestamp(1696000704)),
+        (SpecId::CANCUN, ForkCondition::Timestamp(1707305664)),
+        (SpecId::PRAGUE, ForkCondition::Timestamp(1740434112)),
+    ]),
+});
+
+/// The Ethereum Mainnet [ChainSpec].
+pub static ETH_MAINNET_CHAIN_SPEC: LazyLock<EthChainSpec> = LazyLock::new(|| ChainSpec {
+    chain_id: 1,
+    forks: BTreeMap::from([
+        (SpecId::MERGE, ForkCondition::Block(15537394)),
+        (SpecId::SHANGHAI, ForkCondition::Timestamp(1681338455)),
+        (SpecId::CANCUN, ForkCondition::Timestamp(1710338135)),
+        (SpecId::PRAGUE, ForkCondition::Timestamp(1746612311)),
+    ]),
+});
+
+/// [ChainSpec] for a custom Steel Testnet using the Prague EVM.
+pub static STEEL_TEST_PRAGUE_CHAIN_SPEC: LazyLock<ChainSpec<SpecId>> =
+    LazyLock::new(|| ChainSpec::new_single(5733100018, SpecId::PRAGUE));
+
+/// [EvmFactory] for Ethereum.
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
+#[non_exhaustive]
+pub struct EthEvmFactory;
+
+impl EvmFactory for EthEvmFactory {
+    type Evm<DB: Database> = <AlloyEthEvmFactory as AlloyEvmFactory>::Evm<DB, NoOpInspector>;
+    type Tx = <AlloyEthEvmFactory as AlloyEvmFactory>::Tx;
+    type Error<DBError: Error + Send + Sync + 'static> =
+        <AlloyEthEvmFactory as AlloyEvmFactory>::Error<DBError>;
+    type HaltReason = <AlloyEthEvmFactory as AlloyEvmFactory>::HaltReason;
+    type Spec = <AlloyEthEvmFactory as AlloyEvmFactory>::Spec;
+    type Header = EthBlockHeader;
+
+    fn new_tx(address: Address, data: Bytes) -> Self::Tx {
+        TxEnv {
+            caller: address,
+            kind: TxKind::Call(address),
+            data,
+            chain_id: None,
+            ..Default::default()
+        }
+    }
+
+    fn create_evm<DB: Database>(
+        db: DB,
+        chain_id: u64,
+        spec: Self::Spec,
+        header: &Self::Header,
+    ) -> Self::Evm<DB> {
+        let mut cfg_env = CfgEnv::new_with_spec(spec).with_chain_id(chain_id);
+        cfg_env.disable_nonce_check = true;
+        cfg_env.disable_balance_check = true;
+        cfg_env.disable_block_gas_limit = true;
+        // Disabled because eth_call is sometimes used with eoa senders
+        cfg_env.disable_eip3607 = true;
+        // The basefee should be ignored for eth_call
+        cfg_env.disable_base_fee = true;
+
+        let block_env = header.to_block_env(spec);
+
+        AlloyEthEvmFactory::default().create_evm(db, (cfg_env, block_env).into())
+    }
+}
+
+/// [ChainSpec] for Ethereum.
+pub type EthChainSpec = ChainSpec<SpecId>;
+
+/// [EvmEnv] for Ethereum.
+pub type EthEvmEnv<D, C> = EvmEnv<D, EthEvmFactory, C>;
+
+/// [EvmInput] for Ethereum.
+pub type EthEvmInput = EvmInput<EthEvmFactory>;
+
+/// [EvmBlockHeader] for Ethereum.
+pub type EthBlockHeader = RlpHeader<alloy_consensus::Header>;
+
+impl EvmBlockHeader for EthBlockHeader {
+    type Spec = SpecId;
+
+    #[inline]
+    fn parent_hash(&self) -> &B256 {
+        &self.inner().parent_hash
+    }
+    #[inline]
+    fn number(&self) -> BlockNumber {
+        self.inner().number
+    }
+    #[inline]
+    fn timestamp(&self) -> u64 {
+        self.inner().timestamp
+    }
+    #[inline]
+    fn state_root(&self) -> &B256 {
+        &self.inner().state_root
+    }
+    #[inline]
+    fn receipts_root(&self) -> &B256 {
+        &self.inner().receipts_root
+    }
+    #[inline]
+    fn logs_bloom(&self) -> &alloy_primitives::Bloom {
+        &self.inner().logs_bloom
+    }
+
+    #[inline]
+    fn to_block_env(&self, spec: SpecId) -> BlockEnv {
+        let header = self.inner();
+
+        let blob_excess_gas_and_price = header.excess_blob_gas.map(|excess_blob_gas| match spec {
+            SpecId::CANCUN => BlobExcessGasAndPrice::new(
+                excess_blob_gas,
+                eip4844::BLOB_GASPRICE_UPDATE_FRACTION as u64,
+            ),
+            SpecId::PRAGUE => BlobExcessGasAndPrice::new(
+                excess_blob_gas,
+                eip7691::BLOB_GASPRICE_UPDATE_FRACTION_PECTRA as u64,
+            ),
+            SpecId::OSAKA => BlobExcessGasAndPrice::new(
+                excess_blob_gas,
+                eip7691::BLOB_GASPRICE_UPDATE_FRACTION_PECTRA as u64,
+            ),
+            _ => unimplemented!("unsupported spec with `excess_blob_gas`: {spec}"),
+        });
+
+        BlockEnv {
+            number: U256::from(header.number),
+            beneficiary: header.beneficiary,
+            timestamp: U256::from(header.timestamp),
+            gas_limit: header.gas_limit,
+            basefee: header.base_fee_per_gas.unwrap_or_default(),
+            difficulty: header.difficulty,
+            prevrandao: (spec >= SpecId::MERGE).then_some(header.mix_hash),
+            blob_excess_gas_and_price,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloy::primitives::b256;
+
+    use super::{
+        ETH_HOLESKY_CHAIN_SPEC, ETH_MAINNET_CHAIN_SPEC, ETH_SEPOLIA_CHAIN_SPEC,
+        STEEL_TEST_PRAGUE_CHAIN_SPEC,
+    };
+
+    // NOTE: If these are updated here, make sure to update them in Steel.sol
+
+    #[test]
+    fn mainnet_spec_digest() {
+        assert_eq!(
+            ETH_MAINNET_CHAIN_SPEC.digest(),
+            b256!("0x9a223c7ca04c969f1cacbe5b8db44c308b2c53390505d3d48c834ed4469fc839")
+        );
+    }
+
+    #[test]
+    fn sepolia_spec_digest() {
+        assert_eq!(
+            ETH_SEPOLIA_CHAIN_SPEC.digest(),
+            b256!("0x5c9552dc9bfad8572ded4f818bb35b0f4260660c1554236986b768ae999b4b60")
+        );
+    }
+
+    #[test]
+    fn holesky_spec_digest() {
+        assert_eq!(
+            ETH_HOLESKY_CHAIN_SPEC.digest(),
+            b256!("0x8eae1ba5f877e6ad7007bf6985f5245be7d758457fb4eb7e6a72d47f49bea389")
+        );
+    }
+
+    #[test]
+    fn testnet_spec_digest() {
+        assert_eq!(
+            STEEL_TEST_PRAGUE_CHAIN_SPEC.digest(),
+            b256!("0x33e32d9590cd4b168773ca27de65d535f2e744274b1437acb712dd4264f2eb87")
+        );
+    }
+}
